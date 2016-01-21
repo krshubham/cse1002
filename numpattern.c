aeed20d13ef2e197e7bb9b93acab762b005794fc
Kumar Shubham <kumarshubham347@gmail.com>
2016-01-21 09:32:34 +0530

    All the code updated

---
+++ numpattern.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+	int i,j;
+	int n=6;
+	for(i=0;i<n;i++){
+		for(j=0;j<=i;j++){
+			printf("%d ",j );
+		}
+		printf("\n");
+		if(i==5){
+			for(i;i<2n;i++){
+				for(j=0;j<=i;j++){
+					printf("j ");
+				}
+				printf("%d\n", );
+			}
+		}
+	}
+
+	}
+	return 0;
+}

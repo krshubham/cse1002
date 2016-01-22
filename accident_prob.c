d2b359726ffed2a454ddec193595bbe153450e5f
Kumar Shubham <kumarshubham347@gmail.com>
2016-01-22 12:33:10 +0530

    Added the huffman coding problem

--- accident_prob.c
+++ accident_prob.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
+#include <math.h>
 int main(int argc, char const *argv[])
 {
-	int n;
-	printf("Enter the number of days you wnat to record: ");
+	int n,i;
+	float a=0;
+	float total=0;
+	float average;
+	printf("Enter the number of days you want to record: ");
 	scanf("%d",&n);
-	int num[n];
-	average()
-	return 0;
-}
-int average(array[int n]){
-	int total;
-	float avg;
-	for(i=0;i<n;i++){
-		total+=array[i];
+	int accarray[n];
+	for(i=0; i<n;i++){
+		printf("Enter the number of accident(s) on day %d: ",i+1);
+		scanf("%d",&accarray[i]);
 	}
-	avg = total/n ;
-	return avg;
+	for(i=0;i<n;i++){
+		total+=accarray[i];
+	}
+	average = total/n;
+	printf("The average is: %f\n",average);
+	for(i=0;i<n;i++){
+		a = fabsf(average - accarray[i]);
+		printf("The difference on day %d is: %f\n",i+1,a);
+		a=0;
+	}
+	return 0;
 }
---
+++ count_duplicate.c
@@ -0,0 +1,19 @@
+//huffman coding problem
+/*
+Question:
+Develop an algorithm to find the frequency of each character in C.
+*/
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+	/*int i=0,n;
+	int a;
+	int arr_size;
+	char str[10];
+    printf("Enter the string: ");
+    a = scanf("%c",str);
+    //printf("Your string is: %s",str);
+    printf("%d\n",a);
+    */
+
+}

4ee66b12369c324d9ddd84b8ef7d56e69acb5f46
Kumar Shubham <kumarshubham347@gmail.com>
2016-01-22 13:13:45 +0530

    Added all the problems

--- count_duplicate.c
+++ count_duplicate.c
@@ -1,19 +1,20 @@
 //huffman coding problem
-/*
-Question:
+/*Question:
 Develop an algorithm to find the frequency of each character in C.
 */
 #include <stdio.h>
 int main(int argc, char const *argv[])
 {
-	/*int i=0,n;
-	int a;
-	int arr_size;
-	char str[10];
-    printf("Enter the string: ");
-    a = scanf("%c",str);
-    //printf("Your string is: %s",str);
-    printf("%d\n",a);
-    */
-
+	char str[30];
+	int i;
+    printf("Enter string: ");
+    gets(str);     //Read string from user.
+    printf("str: ");
+    puts(str);
+    for (i = 0; i < 30; i++)
+    {
+    	printf("%c ",str[i] );
+    }
+    printf("\n");
+    return 0;
 }

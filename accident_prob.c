8b4132f59bc5c9e9150d4baadf66971df6bfb7e5
Kumar Shubham <kumarshubham347@gmail.com>
2016-01-22 11:54:20 +0530

    Added the code for accident problems

---
+++ accident_prob.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+	int n;
+	printf("Enter the number of days you wnat to record: ");
+	scanf("%d",&n);
+	int num[n];
+	average()
+	return 0;
+}
+int average(array[int n]){
+	int total;
+	float avg;
+	for(i=0;i<n;i++){
+		total+=array[i];
+	}
+	avg = total/n ;
+	return avg;
+}
